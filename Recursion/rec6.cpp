039cb84e78046c2d08fca828c309e9b089d78589
Ashish Yadav <ay079396@gmail.com>
2022-05-23 13:31:46 +0530

    improving the code

--- Recursion/rec6.cpp
+++ Recursion/rec6.cpp
@@ -3,20 +3,15 @@ using namespace std;
 
 void f(int i,int arr[],int n){
     if(i>=n/2) return;
-    swap(arr[i],arr[n]);
-    f(i+1,arr,n-i-1);
-
+    swap(arr[i],arr[n-i-1]);
+    f(i+1,arr,n);
 }
-
-
 int main(){
     int n;
     cin>>n;
     int arr[n];
     for (int i = 0; i < n; i++)
-    {
         cin>>arr[i];
-    }
     
     f(0, arr,n);
     for (int i = 0; i < n ;i++) 
