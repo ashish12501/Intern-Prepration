d8de10d290fab8b241462a75aaf34127cb021b2e
Ashish Yadav <ay079396@gmail.com>
2022-05-30 17:53:13 +0530

    optimised code for setmatrixzeroes problem

--- Array1/SetMatrixZeroes.cpp
+++ Array1/SetMatrixZeroes.cpp
@@ -5,42 +5,22 @@ int main()
 {
     vector<vector<int>> matrix{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
     // vector<vector<int>> matrix {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
-    for (int i = 0; i < matrix.size(); i++)
-    {
-        for (int j = 0; j < matrix[i].size(); j++)
-        {
-            if (matrix[i][j] == 0)
-            {
-                int row = i;
-                int col = j;
-                for (int k = 0; k < matrix.size(); k++)
-                {
-                    for (int l = 0; l < matrix[i].size(); l++)
-                    {
-                        if ((k == row || l == col) && matrix[k][l] != 0)
+    // vector<vector<int>> matrix {{1,1,1,1},{1,0,1,1},{1,1,0,1},{0,0,0,1}};
+    // vector<vector<int>> matrix{{1,0,3}};
 
-                            matrix[k][l] = -99;
-                    }
-                }
-            }
-        }
+    int col0=1,rows=matrix.size(),cols=matrix[0].size();
+    for(int i=0;i<rows;i++){
+     if(matrix[i][0]==0) 
+      col0=0;
+     for (int j = 1; j < cols; j++)
+      if(matrix[i][j]==0)
+       matrix[i][0]=matrix[0][j]=0;
+    }  
+    for (int i = rows-1; i>=0; i--){
+    for (int j = cols-1; j>=1; j--)
+        if(matrix[i][0] ==0 || matrix[0][j] == 0)
+           matrix[i][j]=0;
+    if(col0==0) matrix[i][0]=0;
     }
-
-    for (int m = 0; m < matrix.size(); m++)
-    {
-        for (int n = 0; n < matrix[m].size(); n++)
-        {
-            if (matrix[m][n] == -99)
-                matrix[m][n] = 0;
-        }
-    }
-
-    for (int i = 0; i < matrix.size(); i++)
-    {
-        for (int j = 0; j < matrix[i].size(); j++)
-            cout << matrix[i][j] << " ";
-        cout << endl;
-    }
-    cout<<endl<<endl;;
-
+    
 }
